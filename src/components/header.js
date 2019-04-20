e8e75a5d0f86f840e9c0348d1db674b4594e236c
Ryan Barley <rmbarley@gmail.com>
2019-04-20 16:40:50 -0400

    Style header, Add RSVP form

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,20 @@
-import { Link } from "gatsby"
 import React from "react"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
 
-const Header = () => <header>This is the header</header>
+const Header = () => (
+  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+    <Navbar.Brand href="#home">Deerly Beloved</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className="ml-auto">
+        <Nav.Link href="#">Us</Nav.Link>
+        <Nav.Link href="#">Photos</Nav.Link>
+        <Nav.Link href="#rsvp-form">RSVP</Nav.Link>
+        <Nav.Link href="#">Info</Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
 
 export default Header
